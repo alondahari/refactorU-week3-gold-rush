225e4f79753472de6af009723cb869916b377a0b
Alon Dahari <climbinghobo@gmail.com>
2015-01-27 11:56:05 -0700

    add notes

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 $(document).on('ready', function() {
 
-    // $('.marker').on('click', function(e){
-    //   this.detach();
-    // });
-
     $('.container').on('click', function(e) {
 
+        // check if a marker was clicked
         if ($(e.target).hasClass('marker')){
           $(e.target).remove();
           return false;
         }
-        var cordX = e.pageX;
-        var cordY = e.pageY;
+
         var marker = $('<div class="marker">X</div>');
         marker.css({
-            top: cordY,
-            left: cordX
+            top: e.pageY,
+            left: e.pageX
         });
         $(this).append(marker);
+
+        var addNote = $('<input placeholder="Enter note">').addClass('addNote');
+
+        $(addNote).appendTo('body').focus().on({
+          'blur': function(){
+            marker.append('<p>' + this.value);
+
+
+            this.remove();
+          },
+          'keyup': function (e) {
+            if (e.keyCode == 13) {
+              this.blur();
+            }
+          }
+
+        });
     });
+
+
 
 });
