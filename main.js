3cb58959feae3d845ff687b6565347ab0dd1ef54
Charlie Fox <webartificer@gmail.com>
2015-01-27 11:01:03 -0700

    added marker

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).on('ready', function() {
-  
+
+    $('.container').on('click', function(e) {
+        var cordX = e.pageX;
+        var cordY = e.pageY;
+        var marker = $('<div class="marker">');
+        marker.css({
+            top: cordY,
+            left: cordX
+        });
+        $(this).append(marker);
+    });
+
+
+
 });
