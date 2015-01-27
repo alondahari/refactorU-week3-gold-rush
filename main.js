e168d0d8a4f5a377b5cb26bcb5aee152bad8689c
Alon Dahari <climbinghobo@gmail.com>
2015-01-27 11:22:00 -0700

    add marker remove

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 $(document).on('ready', function() {
 
+    // $('.marker').on('click', function(e){
+    //   this.detach();
+    // });
+
     $('.container').on('click', function(e) {
+
+        if ($(e.target).hasClass('marker')){
+          $(e.target).remove();
+          return false;
+        }
         var cordX = e.pageX;
         var cordY = e.pageY;
-        var marker = $('<div class="marker">');
+        var marker = $('<div class="marker">X</div>');
         marker.css({
             top: cordY,
             left: cordX
         });
         $(this).append(marker);
     });
-
-
 
 });
